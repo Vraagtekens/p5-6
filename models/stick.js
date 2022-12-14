9f641d7e80f381013c52a3af8cc626a4e5aa0cd2
Dylan Jansen <dylan.jansen4@hva.nl>
2022-12-14 19:45:35 +0100

    p5-2

---
+++ models/stick.js
@@ -0,0 +1,28 @@
+class Stick{
+    x
+    y
+    
+    constructor(x, y, color){
+        this.x = x
+        this.y = y
+        this.color = color
+    }
+
+    draw(){
+        push();
+        //Transform shape
+        rectMode(CENTER)
+        translate(this.x, this.y)
+        rotate(angle + this.x + this.y )
+    
+        //Draw shape
+        noStroke()
+        fill(255)
+        rect(0, 0, 30, 5, 20)
+        pop();
+    }
+
+
+
+}
+
